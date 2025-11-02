9477bd4cdc193a54b666868644003e1f3e80a3b9
Buharin Daniil <danilbuharin1@gmail.com>
2025-11-02 20:19:54 +0700

    Header Implemented,The beginning of cpp(version1)

--- PasswordGenerator.cpp
+++ PasswordGenerator.cpp
@@ -1,5 +1,23 @@
-//
-// Created by danil on 02.11.2025.
-//
-
+#include "iostream"
 #include "PasswordGenerator.h"
+#include "stdexcept"
+
+PasswordGenerator::PasswordGenerator(int len, bool uppercase, bool lowercase,
+                                     bool digits, bool special)
+        : length(len),
+          use_uppercase(uppercase),
+          use_lowercase(lowercase),
+          use_digits(digits),
+          use_special_chars(special) {
+
+    std::random_device rd;
+    random_engine = std::mt19937(rd());
+
+    // Проверка корректности настроек
+    validateSettings();
+}
+
+std::string PasswordGenerator::generate() {
+    validateSettings();
+}
+
--- PasswordGenerator.h
+++ PasswordGenerator.h
@@ -1,14 +1,63 @@
-//
-// Created by danil on 02.11.2025.
-//
+#ifndef PASSWORDGENERATOR_HPP
+#define PASSWORDGENERATOR_HPP
 
-#ifndef IRONVAULT_MANAGER_PASSWORDGENERATOR_H
-#define IRONVAULT_MANAGER_PASSWORDGENERATOR_H
-
+#include "string"
+#include <random>
 
 class PasswordGenerator {
+private:
+    int length; // длина пароля
+    bool use_uppercase; // Использвать заглавные буквы
+    bool use_lowercase; // Использвать строчные буквы
+    bool use_digits; // Использват цифры
+    bool use_special_chars; // Использовать спец символы
+    std::mt19937 random_engine;   // генератор случайных чисел
 
+public:
+    PasswordGenerator(int len = 12,
+                      bool uppercase = true,
+                      bool lowercase = true,
+                      bool digits = true,
+                      bool special = false);
+
+    std::string generate();
+
+    // Настройка параметров
+    void setLength(int len);
+
+    void setUppercase(bool use);
+
+    void setLowercase(bool use);
+
+    void setDigits(bool use);
+
+    void setSpecialChars(bool use);
+
+    // Получение параметров
+    int getLength() const;
+
+    bool getUppercase() const;
+
+    bool getLowercase() const;
+
+    bool getDigits() const;
+
+    bool getSpecialChars() const;
+
+private:
+    // Вспомогательные методы
+    std::string getUpperChars() const;
+
+    std::string getLowercaseChars() const;
+
+    std::string getDigitChars() const;
+
+    std::string getSpecialCharsSet() const;
+
+    std::string getAllAvailableChars() const;
+
+    void validateSettings() const;
 };
 
 
-#endif //IRONVAULT_MANAGER_PASSWORDGENERATOR_H
+#endif
