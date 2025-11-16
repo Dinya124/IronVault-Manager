5e738008bfb0ad40c7405b18e70dc2b4a0649088
Buharin Daniil <danilbuharin1@gmail.com>
2025-11-16 17:35:05 +0700

    generating a password key

--- DataEncryption .h
+++ DataEncryption .h
@@ -6,12 +6,24 @@
 #include <openssl/evp.h>
 #include <openssl/rand.h>
 
-class DataEncryption{
+class DataEncryption {
 private:
     static const size_t KEY_LENGTH = 32;
     static const size_t IV_LENGTH = 16;
     static const size_t SALT_LENGTH = 16;
     static const int ITERATIONS = 100000;
+
+public:
+    // Основные методы шифрования/дешифрования
+    static std::string
+    encrypt(const std::string &plaintext, const std::string &password, const std::string &internal_key = "");
+
+    static std::string
+    decrypt(const std::string &ciphertext, const std::string &password, const std::string &internal_key = "");
+
+    // Генерация ключа пароля
+    static std::vector<unsigned char> deriveKey(const std::string &password, const std::vector<unsigned char> &salt,
+                                                const std::string &internal_key = "");
 
 };
 
