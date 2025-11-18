48665a8efeb8b3aa33fb9ce1b6500acc9cd33cba
Buharin Daniil <danilbuharin1@gmail.com>
2025-11-19 02:15:56 +0700

    first commit

---
+++ MasterPasswordManager.h
@@ -0,0 +1,15 @@
+#ifndef IRONVAULT_MANAGER_MASTERPASSWORDMANAGER_H
+#define IRONVAULT_MANAGER_MASTERPASSWORDMANAGER_H
+
+#include <string>
+#include <vector>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+
+
+class MasterPasswordManager {
+
+};
+
+
+#endif //IRONVAULT_MANAGER_MASTERPASSWORDMANAGER_H
