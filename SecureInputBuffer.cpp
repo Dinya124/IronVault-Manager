5f0f4afa146e982131e3ea2cbbc250604a0ac34a
Buharin Daniil <danilbuharin1@gmail.com>
2025-11-20 22:45:26 +0700

    input display, security checks

--- SecureInputBuffer.cpp
+++ SecureInputBuffer.cpp
@@ -6,12 +6,14 @@
 
 // Для системно-зависимых функций
 #ifdef _WIN32
+
 #include <windows.h>
 #include <conio.h>
+
 #else
 #include <termios.h>
-    #include <unistd.h>
-    #include <sys/ioctl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
 #endif
 
 // Конструктор по умолчанию
@@ -37,7 +39,8 @@ bool SecureInputBuffer::readFromStdin(bool hide_input) {
 }
 
 // Чтение с callback для прогресса
-bool SecureInputBuffer::readFromStdinWithCallback(const std::function<void(size_t, size_t)>& callback, bool hide_input) {
+bool
+SecureInputBuffer::readFromStdinWithCallback(const std::function<void(size_t, size_t)> &callback, bool hide_input) {
     clear();
 
     // Сохраняем текущие настройки терминала
@@ -58,16 +61,13 @@ bool SecureInputBuffer::readFromStdinWithCallback(const std::function<void(size_
             if (c == ENTER) {
                 std::cout << std::endl;
                 break;
-            }
-            else if (c == BACKSPACE) {
+            } else if (c == BACKSPACE) {
                 handleBackspace();
-            }
-            else if (c == CTRL_C || c == CTRL_D) {
+            } else if (c == CTRL_C || c == CTRL_D) {
                 secureClear();
                 setStdinEcho(original_echo);
                 return false; // Прервано пользователем
-            }
-            else if (isValidCharacter(c)) {
+            } else if (isValidCharacter(c)) {
                 handleCharacter(c, hide_input);
             }
 
@@ -88,11 +88,12 @@ bool SecureInputBuffer::readFromStdinWithCallback(const std::function<void(size_
 
         return true;
 
-    } catch (const std::exception& e) {
+    } catch (const std::exception &e) {
         setStdinEcho(original_echo);
         throw;
     }
 }
+
 // Очистка буфера
 void SecureInputBuffer::clear() {
     std::fill(buffer.begin(), buffer.end(), 0);
@@ -105,7 +106,7 @@ void SecureInputBuffer::secureClear() {
     position = 0;
 
     // Дополнительная гарантия очистки
-    for (auto& c : buffer) {
+    for (auto &c: buffer) {
         c = 0;
     }
 }
@@ -144,4 +145,44 @@ bool SecureInputBuffer::isEmpty() const {
 // Получение емкости буфера
 size_t SecureInputBuffer::getCapacity() const {
     return buffer.size();
+}
+
+// Настройка отображения ввода
+void SecureInputBuffer::setEchoEnabled(bool enabled) {
+    echo_enabled = enabled;
+}
+
+void SecureInputBuffer::setMaskChar(char mask) {
+    mask_char = mask;
+}
+
+bool SecureInputBuffer::isEchoEnabled() const {
+    return echo_enabled;
+}
+
+char SecureInputBuffer::getMaskChar() const {
+    return mask_char;
+}
+
+// Проверки безопасности
+bool SecureInputBuffer::containsNullBytes() const {
+    for (size_t i = 0; i < position; ++i) {
+        if (buffer[i] == '\0') {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool SecureInputBuffer::exceedsMaxSize() const {
+    return position >= MAX_BUFFER_SIZE;
+}
+
+bool SecureInputBuffer::isValidUtf8() const {
+    for (size_t i = 0; i < position; ++i) {
+        if (static_cast<unsigned char>(buffer[i]) > 127) {
+            return true; // Упрощенная проверка
+        }
+    }
+    return true;
 }
