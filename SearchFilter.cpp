64bd0660c07b822d3a7baf80c3d11a8d133cd3e5
Buharin Daniil <danilbuharin1@gmail.com>
2025-11-20 18:41:26 +0700

    default constructor

--- SearchFilter.cpp
+++ SearchFilter.cpp
@@ -1,5 +1,19 @@
-//
-// Created by danil on 19.11.2025.
-//
-
 #include "SearchFilter.h"
+#include <algorithm>
+#include <cctype>
+#include <ctime>
+
+// Конструктор по умолчанию
+SearchFilter::SearchFilter()
+        : service_name_query(""),
+          login_query(""),
+          url_query(""),
+          category_query(""),
+          notes_query(""),
+          case_sensitive(false),
+          exact_match(false),
+          search_in_notes(false),
+          date_from(0),
+          date_to(0) {
+}
+
